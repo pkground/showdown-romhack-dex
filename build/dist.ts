cbd4a9cb1f483520eb332f4a46762bc99cd37c27
pkground <iduh708@gmail.com>
2025-05-03 23:47:54 +0100

    4th try...!

--- build/dist.ts
+++ build/dist.ts
@@ -1,15 +1,20 @@
 import { execSync } from "child_process";
 import { argv, exitCode } from "process";
-import config from "../data/config.json"
+import config from "../data/config.json";
+import * as fs from "fs";
 
 let dest = argv[2];
 
-execSync(`mkdir -p ./${dest}`);
+if (!fs.existsSync(`./${dest}`)) {
+    fs.mkdirSync(`./${dest}`, { recursive: true });
+}
+
 execSync(`rm -rf ./${dest}/*`);
 
 for (let folder of ["images"]) {
-  execSync(`cp -rf ${folder} ./${dest}/`);
+    execSync(`cp -rf ${folder} ./${dest}/`);
 }
-execSync(`echo $PWD`)
-execSync(`npx parcel build --public-url ${config.baseurl} ./index.html`)
+
+execSync(`echo $PWD`);
+execSync(`npx parcel build --public-url ${config.baseurl} ./index.html`);
 execSync(`cp ./${dest}/index.html ./${dest}/404.html`);
