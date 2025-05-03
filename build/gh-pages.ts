152dbb7572ed36984d31ae81d2ec864c3456eee9
pkground <iduh708@gmail.com>
2025-05-04 00:23:12 +0100

    dist-contents time... apparently might work

--- build/gh-pages.ts
+++ build/gh-pages.ts
@@ -1,5 +1,5 @@
 import * as ghpages from "gh-pages";
 
 ghpages.publish("dist", {
-  repo: "https://github.com/pkground/showdown-romhack-dex",
+  repo: "https://github.com/pkground/dist-contents",
 });
